4cb0d736e3800e88a4200aee24fb87abef5270af
Ikevine <ishimwekevine098@gmail.com>
2023-04-20 18:22:32 +0200

    update sanity

---
+++ backend/studio/static/index-220b3c7a-38c54891.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-98d70598-fc5240d0.js";import"./sanity-028c7275.js";import"./index-4ca0d66d.js";export{m as DocumentPaneProvider,a as default};
